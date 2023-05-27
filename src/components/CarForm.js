a9881622331ced97d30e51c111ac1c8575574572
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-05-27 08:03:02 -0400

    Update CarForm

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,37 @@
+import { useDispatch } from "react-redux";
+import { changeName } from "../store";
+
 function CarForm() {
-  return <div>CarForm</div>;
+  
+  const dispatch = useDispatch();
+
+  const handleNameChange = (event) => {
+      dispatch(changeName(event.target.value));
+
+  };
+
+
+
+
+
+
+  return <div className="car-form panel">
+    <h4 className="subtitle is-3">
+      Add Car 
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input className="input is-expanded"
+            value={name}
+            onChange={handleNameChange} />
+          </div>
+
+        </div>
+      </form>
+    </h4>
+
+  </div>;
 }
 
 export default CarForm;
