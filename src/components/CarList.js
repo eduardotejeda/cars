516d8c669eea64ff64256055e9be7328ac0dcb5b
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-05-29 21:54:48 -0400

    Update logic for AddCar component

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,37 +1,38 @@
-import { useSelector, useDispatch } from "react-redux";
-import { removeCar } from "../store";
-
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store';
 
 function CarList() {
   const dispatch = useDispatch();
   const cars = useSelector((state) => {
     return state.cars.data;
-
   });
 
-const handleCarDelete = (car) => {
-dispatch(removeCar(car.id))
-};
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
 
   const renderedCars = cars.map((car) => {
     return (
       <div key={car.id} className="panel">
         <p>
-          {car.name} - ${car.name}
+          {car.name} - ${car.cost}
         </p>
-        <button className=" button is-danger"
-        onClick={()=> handleCarDelete(car)}
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
         >
-          Delete</button>
+          Delete
+        </button>
       </div>
     );
-
   });
 
-  return <div className="car-list">
-    {renderedCars}
-    <hr />
-    </div>;
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
